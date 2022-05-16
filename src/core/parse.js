45cb599c1de01fcad81bd1723c222e729fb47720
PinghuaZhuang <zphua2016@gmail.com>
2022-05-16 23:22:29 +0800

    feat: add callbacks.

--- src/core/parse.js
+++ src/core/parse.js
@@ -1,45 +1,49 @@
 import fs from "fs";
 import path from "path";
-import partial from 'lodash/partial'
+import partial from "lodash/partial";
 import * as parser from "@babel/parser";
 import traverse from "@babel/traverse";
 // import t from "@babel/types";
-import Token from './utils/Token';
+import Callbacks from "../utils/Callbacks";
+import Token from "../utils/Token";
 
-const rnothtmlwhite = /[\x20\t\r\n\f]+/
+const rnothtmlwhite = /[\x20\t\r\n\f]+/;
 
 function createVisitors(visitors, handle, ...args) {
   const ret = {};
   visitors
     .trim()
     .split(rnothtmlwhite)
-    .forEach(visitor => {
+    .forEach((visitor) => {
       ret[visitor] = partial(handle, ...args);
     });
   return ret;
 }
 
-function findZnString(tokens, { node }) {
+function findZhCNString(tokens, { node }) {
   const reg = /(?<content>[\u4e00-\u9fa5]+)/g;
   let t;
   while ((t = reg.exec(node.value))) {
-    console.log(t.groups.content);
-    tokens.push(t.groups.content);
+    tokens.add(() => console.count());
   }
 }
 
-function fnExpHandle(tokens, path) {
-  path.traverse(createVisitors(`
-    TemplateLiteral
-    JSXText
-    Literal
-  `, findZnString, tokens));
+function findFunction(tokens, path) {
+  path.traverse(
+    createVisitors(`
+      TemplateLiteral
+      JSXText
+      Literal`,
+      findZhCNString,
+      tokens
+    )
+  );
   path.skip();
 }
 
-// TODO: async progress
 function parse(filePath) {
-  const tokens = []; // callbacks
+  const tokens = Callbacks("memory once"); // callbacks
+  tokens.fire();
   const content = fs.readFileSync(path.join(process.cwd(), filePath), "utf8");
 
   const ast = parser.parse(content, {
@@ -47,12 +51,16 @@ function parse(filePath) {
     allowImportExportEverywhere: true,
   });
 
-  traverse(ast, createVisitors(`
-    ArrowFunctionExpression
-    FunctionExpression
-  `, fnExpHandle, tokens));
+  traverse(
+    ast,
+    createVisitors(
+      `ArrowFunctionExpression FunctionExpression`,
+      findFunction,
+      tokens
+    )
+  );
 
-  console.log(tokens, 'tokens');
+  console.log(tokens, "tokens");
 }
 
 parse("src/mock/index.jsx");
--- src/utils/Token.js
+++ src/utils/Token.js
@@ -1,5 +1,23 @@
-class Token {
+import merge from "lodash/merge";
+import youdao from '../service/youdao';
 
+const defaultOptions = {
+  to: "en",
+  translateService: "youdao",
+};
+
+class Token {
+  constructor(zh, options = {}) {
+    this.literal = zh;
+    this.options = merge({}, defaultOptions, options);
+  }
+
+  translate() {
+    return youdao({
+      q: this.literal,
+      to: 'en',
+    });
+  }
 }
 
 export default Token;
